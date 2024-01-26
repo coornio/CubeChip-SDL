19517b7d34fc6540a939f6ce7c0a41656bbdf541
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2024-01-26 15:05:14 +0200

    clean up init's ifstream

--- src/GuestClass/Init.cpp
+++ src/GuestClass/Init.cpp
@@ -165,11 +165,7 @@ bool VM_Guest::romSizeCheck(const u32 size, const u16 offset) {
     Mem.ram.resize(size);       // resize the memory vector
 
     std::ifstream ifs(Host.File.path, std::ios::binary);
-    if (ifs.read(
-        to<char*>(Mem.ram.data() + offset),
-        Host.File.size
-    )) return true;
-    return false;
+    return as<bool>(ifs.read(to<char*>(Mem.ram.data() + offset), Host.File.size));
 }
 
 void VM_Guest::initPlatform() {
