e77af19e682adb153509d3cc181cd1fd6909ab91
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2024-05-19 21:47:15 +0300

    Framelimiter's now got optional arguments for setLimiter(), and operator() now is check() instead, as it wasn't really descriptive whatsoever before

--- src/Assistants/FrameLimiter.cpp
+++ src/Assistants/FrameLimiter.cpp
@@ -1,67 +1,62 @@
 /*
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #include "FrameLimiter.hpp"
 
-FrameLimiter::FrameLimiter(
-    const double framerate,
-    const bool   firstpass,
-    const bool   lostframe
+void FrameLimiter::setLimiter(
+	const double              framerate,
+	const std::optional<bool> firstpass,
+	const std::optional<bool> lostframe
 ) {
-    setFreq(framerate, firstpass, lostframe);
+	timeFrequency = 1000.0 / std::clamp(framerate, 0.5, 1000.0);
+	if (firstpass) skipFirstPass = *firstpass;
+	if (lostframe) skipLostFrame = *lostframe;
 }
 
-void FrameLimiter::setFreq(
-    const double framerate,
-    const bool   firstpass,
-    const bool   lostframe
-) {
-    timeFrequency = 1000.0 / std::clamp(framerate, 0.5, 1000.0);
-    skipFirstPass = firstpass;
-    skipLostFrame = lostframe;
-}
+bool FrameLimiter::check(const bool mode) {
+	if (isValidFrame()) return true;
 
-bool FrameLimiter::operator()(const bool mode) {
-    if (isValidFrame()) return true;
-
-    if (mode == SLEEP && remains() >= 2.0)
-        std::this_thread::sleep_for(millis(1));
-    return false;
+	if (mode == SLEEP && remains() >= 2.0) {
+		std::this_thread::sleep_for(millis(1));
+	}
+	return false;
 }
 
 bool FrameLimiter::isValidFrame() {
-    using namespace std::chrono;
+	using namespace std::chrono;
+	const auto timeAtCurrent{ steady_clock::now() };
 
-    if (!initTimeCheck) [[unlikely]] {
-        timePastFrame = steady_clock::now();
-        initTimeCheck = true;
-    }
+	if (!initTimeCheck) [[unlikely]] {
+		timePastFrame = timeAtCurrent;
+		initTimeCheck = true;
+	}
 
-    if (skipFirstPass) [[unlikely]] {
-        skipFirstPass = false;
-        ++validFrameCnt;
-        return true;
-    }
+	if (skipFirstPass) [[unlikely]] {
+		skipFirstPass = false;
+		++validFrameCnt;
+		return true;
+	}
 
-    timeVariation = {
-        timeOvershoot + duration<double, std::milli>
-        (steady_clock::now() - timePastFrame).count()
-    };
+	timeVariation = {
+		timeOvershoot + duration<double, std::milli>
+		(timeAtCurrent - timePastFrame).count()
+	};
 
-    if (timeVariation < timeFrequency) [[likely]]
-        return false;
+	if (timeVariation < timeFrequency) [[likely]] {
+		return false;
+	}
 
-    if (skipLostFrame) {
-        lastFrameLost = timeVariation >= timeFrequency * 1.002f;
-        timeOvershoot = std::fmod(timeVariation, timeFrequency);
-    } else {
-        timeOvershoot = timeVariation - timeFrequency;
-    }
+	if (skipLostFrame) {
+		lastFrameLost = timeVariation >= timeFrequency * 1.002f;
+		timeOvershoot = std::fmod(timeVariation, timeFrequency);
+	} else {
+		timeOvershoot = timeVariation - timeFrequency;
+	}
 
-    timePastFrame = steady_clock::now();
-    ++validFrameCnt;
-    return true;
+	timePastFrame = timeAtCurrent;
+	++validFrameCnt;
+	return true;
 }
--- src/Assistants/FrameLimiter.hpp
+++ src/Assistants/FrameLimiter.hpp
@@ -1,7 +1,7 @@
 /*
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #pragma once
@@ -9,45 +9,55 @@
 #include <chrono>
 #include <ratio>
 #include <thread>
+#include <cstdint>
+#include <optional>
 #include <algorithm>
 
 class FrameLimiter final {
-    using chrono = std::chrono::steady_clock::time_point;
-    using millis = std::chrono::milliseconds;
-    using uint64 = unsigned long long;
+	using chrono = std::chrono::steady_clock::time_point;
+	using millis = std::chrono::milliseconds;
+	using uint64 = std::uint64_t;
 
-    bool   initTimeCheck{}; // forces timestamp update on first check only
-    bool   skipFirstPass{}; // forces valid frame return on first check only
-    bool   skipLostFrame{}; // forces frameskip if timeOvershoot > timeFrequency
-    bool   lastFrameLost{}; // missed frame indicator when frameskip is enabled
+	bool   initTimeCheck{}; // forces timestamp update on first check only
+	bool   skipFirstPass{}; // forces valid frame return on first check only
+	bool   skipLostFrame{}; // forces frameskip if timeOvershoot > timeFrequency
+	bool   lastFrameLost{}; // missed frame indicator when frameskip is enabled
 
-    double timeFrequency{}; // holds time (ms) per unit Hertz
-    double timeOvershoot{}; // holds time remainder (ms) from last successful check
-    double timeVariation{}; // holds time difference between last check and now
-    chrono timePastFrame{}; // holds timestamp of the last frame's check
-    uint64 validFrameCnt{}; // counter of successful frame checks performed
+	double timeFrequency{}; // holds time (ms) per unit Hertz
+	double timeOvershoot{}; // holds time remainder (ms) from last successful check
+	double timeVariation{}; // holds time difference between last check and now
+	chrono timePastFrame{}; // holds timestamp of the last frame's check
+	uint64 validFrameCnt{}; // counter of successful frame checks performed
 
-    bool isValidFrame();
+	bool isValidFrame();
 
 public:
-    FrameLimiter(const FrameLimiter&) = delete;
-    FrameLimiter(
-        const double framerate = 60.0, // 0.5 ... 1000 range
-        const bool   firstpass = true, // skipFirstPass flag
-        const bool   lostframe = true  // skipLostFrame flag
-    );
-    void setFreq(
-        const double framerate, // 0.5 ... 1000 range
-        const bool   firstpass, // skipFirstPass flag
-        const bool   lostframe  // skipLostFrame flag
-    );
+	FrameLimiter(
+		double framerate = 60.0, // 0.5 ... 1000 range
+		bool   firstpass = true, // skipFirstPass flag
+		bool   lostframe = true  // skipLostFrame flag
+	) {
+		setLimiter(framerate, firstpass, lostframe);
+	}
 
-    enum : bool { SPINLOCK, SLEEP };
-    bool operator()(const bool mode = SPINLOCK);
+	FrameLimiter(const FrameLimiter& other)
+		: timeFrequency{ other.timeFrequency }
+		, skipFirstPass{ other.skipFirstPass }
+		, skipLostFrame{ other.skipLostFrame }
+	{}
 
-    uint64 count()   const { return validFrameCnt; }
-    double elapsed() const { return timeVariation; }
-    double remains() const { return timeFrequency - timeVariation; }
-    double percent() const { return timeVariation / timeFrequency; }
-    bool   paced()   const { return timeOvershoot < timeFrequency && !lastFrameLost; }
+	void setLimiter(
+		double              framerate,
+		std::optional<bool> firstpass = std::nullopt,
+		std::optional<bool> lostframe = std::nullopt
+	);
+
+	enum : bool { SPINLOCK, SLEEP };
+	bool   check(bool mode = SPINLOCK);
+
+	uint64 count()   const { return validFrameCnt; }
+	double elapsed() const { return timeVariation; }
+	double remains() const { return timeFrequency - timeVariation; }
+	double percent() const { return timeVariation / timeFrequency; }
+	bool   paced()   const { return timeOvershoot < timeFrequency && !lastFrameLost; }
 };
