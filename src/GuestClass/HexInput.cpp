f6546d41fdb7ef237d31b19ccb29e9162626c869
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2024-05-19 21:48:57 +0300

    drop template requirement from a function that's specialized to begin with, t'was dumb

--- src/GuestClass/HexInput.cpp
+++ src/GuestClass/HexInput.cpp
@@ -1,7 +1,7 @@
 /*
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #include <bit>
@@ -11,56 +11,52 @@
 #include "HexInput.hpp"
 
 HexInput::HexInput()
-    : currentBinds(32)
-    , defaultBinds{
-        {0x1, KEY(1), _}, {0x2, KEY(2), _}, {0x3, KEY(3), _}, {0xC, KEY(4), _},
-        {0x4, KEY(Q), _}, {0x5, KEY(W), _}, {0x6, KEY(E), _}, {0xD, KEY(R), _},
-        {0x7, KEY(A), _}, {0x8, KEY(S), _}, {0x9, KEY(D), _}, {0xE, KEY(F), _},
-        {0xA, KEY(Z), _}, {0x0, KEY(X), _}, {0xB, KEY(C), _}, {0xF, KEY(V), _},
-    }
-{}
+	: currentBinds(32)
+	, defaultBinds{
+		{0x1, KEY(1), _}, {0x2, KEY(2), _}, {0x3, KEY(3), _}, {0xC, KEY(4), _},
+		{0x4, KEY(Q), _}, {0x5, KEY(W), _}, {0x6, KEY(E), _}, {0xD, KEY(R), _},
+		{0x7, KEY(A), _}, {0x8, KEY(S), _}, {0x9, KEY(D), _}, {0xE, KEY(F), _},
+		{0xA, KEY(Z), _}, {0x0, KEY(X), _}, {0xB, KEY(C), _}, {0xF, KEY(V), _},
+	} {}
 
 void HexInput::reset() {
-    setup(defaultBinds);
+	setup(defaultBinds);
 }
 
 void HexInput::refresh() {
-    if (!currentBinds.size()) return;
+	if (!currentBinds.size()) return;
 
-    keysPrev = keysCurr;
-    keysCurr = 0u;
+	keysPrev = keysCurr;
+	keysCurr = 0u;
 
-    for (const KeyInfo& mapping : currentBinds)
-        if (bic::kb.areAnyHeld(mapping.key, mapping.alt))
-            keysCurr |= 1u << mapping.idx;
-    keysLock &= ~(keysPrev ^ keysCurr);
+	for (const auto& mapping : currentBinds)
+		if (bic::kb.areAnyHeld(mapping.key, mapping.alt))
+			keysCurr |= 1u << mapping.idx;
+	keysLock &= ~(keysPrev ^ keysCurr);
 }
 
 void HexInput::setup(const std::vector<KeyInfo>& bindings) {
-    (currentBinds = bindings).resize(bindings.size());
-    keysPrev = keysCurr = keysLock = 0u;
+	(currentBinds = bindings).resize(bindings.size());
+	keysPrev = keysCurr = keysLock = 0u;
 }
 
-template bool HexInput::keyPressed<Uint8>(Uint8&);
+bool HexInput::keyPressed(Uint8& ret) {
+	if (!currentBinds.size()) return false;
 
-template <typename T>
-bool HexInput::keyPressed(T& ret) {
-    if (!currentBinds.size()) return false;
-
-    const auto mask{ keysCurr & ~keysPrev & ~keysLock };
-    if (mask) {
-        ret = static_cast<T>
-            (std::countr_zero(mask & ~(mask - 1u)));
-        keysLock |= mask;
-        return true;
-    }
-    return false;
+	const auto mask{ keysCurr & ~keysPrev & ~keysLock };
+	if (mask) {
+		ret = static_cast<Uint8>
+			(std::countr_zero(mask & ~(mask - 1u)));
+		keysLock |= mask;
+		return true;
+	}
+	return false;
 }
 
 bool HexInput::keyPressed(const std::size_t index, const std::size_t offset) const {
-    return keysCurr & ~keysLock & 1u << ((index & 0xFu) + offset);
+	return keysCurr & ~keysLock & 1u << ((index & 0xFu) + offset);
 }
 
 Uint32 HexInput::currKeys(const std::size_t index) const {
-    return keysLock >> index & 0x1u;
+	return keysLock >> index & 0x1u;
 }
--- src/GuestClass/HexInput.hpp
+++ src/GuestClass/HexInput.hpp
@@ -1,7 +1,7 @@
 /*
-    This Source Code Form is subject to the terms of the Mozilla Public
-    License, v. 2.0. If a copy of the MPL was not distributed with this
-    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 #pragma once
@@ -10,31 +10,30 @@
 #include <vector>
 
 class HexInput final {
-    struct KeyInfo {
-        Uint32       idx; // key index on chip8 pad
-        SDL_Scancode key; // main keyboard equivalent
-        SDL_Scancode alt; // alternative option
-    };
-    
-    static constexpr auto _{ SDL_SCANCODE_UNKNOWN };
+	struct KeyInfo {
+		Uint32       idx; // key index on chip8 pad
+		SDL_Scancode key; // main keyboard equivalent
+		SDL_Scancode alt; // alternative option
+	};
 
-    const std::vector<KeyInfo> defaultBinds;
-          std::vector<KeyInfo> currentBinds;
+	static constexpr auto _{ SDL_SCANCODE_UNKNOWN };
 
-    Uint32 keysCurr{}; // bitfield of key states in current frame
-    Uint32 keysPrev{}; // bitfield of key states in previous frame
-    Uint32 keysLock{}; // bitfield of keys excluded from input checks
+	const std::vector<KeyInfo> defaultBinds;
+	std::vector<KeyInfo> currentBinds;
+
+	Uint32 keysCurr{}; // bitfield of key states in current frame
+	Uint32 keysPrev{}; // bitfield of key states in previous frame
+	Uint32 keysLock{}; // bitfield of keys excluded from input checks
 
 public:
-    explicit HexInput();
+	explicit HexInput();
 
-    void reset();
-    void refresh();
+	void reset();
+	void refresh();
 
-    void setup(const std::vector<KeyInfo>& bindings);
+	void setup(const std::vector<KeyInfo>& bindings);
 
-    template <typename T>
-    bool keyPressed(T& vregister);
-    bool keyPressed(std::size_t index, std::size_t offset) const;
-    Uint32 currKeys(std::size_t index) const;
+	bool keyPressed(Uint8& vregister);
+	bool keyPressed(std::size_t index, std::size_t offset) const;
+	Uint32 currKeys(std::size_t index) const;
 };
