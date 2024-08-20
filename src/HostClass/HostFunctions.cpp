e237cd667ad174f0ace0656546e8c67a0d52a5eb
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2024-08-21 02:26:39 +0300

    more cleanups to the logging, abuse branch prediction to favor the false path of hotkey presses

--- src/HostClass/HostFunctions.cpp
+++ src/HostClass/HostFunctions.cpp
@@ -57,54 +57,58 @@ bool VM_Host::runHost() {
 	while (true) {
 		if (!Frame.checkTime()) { continue; }
 
-		if (eventLoopSDL(Guest, Frame)) {
+		if (eventLoopSDL(Guest, Frame)) [[unlikely]] {
 			return EXIT_SUCCESS;
 		}
 
-		if (kb.isPressed(KEY(RIGHT))) {
+		if (kb.isPressed(KEY(RIGHT))) [[unlikely]] {
 			BAS.changeVolume(+15);
 		}
-		if (kb.isPressed(KEY(LEFT))) {
+		if (kb.isPressed(KEY(LEFT))) [[unlikely]] {
 			BAS.changeVolume(-15);
 		}
 
 		if (GameFileChecker::hasCore()) {
-			if (kb.isPressed(KEY(ESCAPE))) {
+			if (kb.isPressed(KEY(ESCAPE))) [[unlikely]] {
 				BVS.resetWindow();
 				GameFileChecker::delCore();
 				prepareGuest(Guest, Frame);
 				continue;
 			}
-			if (kb.isPressed(KEY(BACKSPACE))) {
+			if (kb.isPressed(KEY(BACKSPACE))) [[unlikely]] {
 				prepareGuest(Guest, Frame);
 				continue;
 			}
-			if (kb.isPressed(KEY(RSHIFT))) {
+			if (kb.isPressed(KEY(RSHIFT))) [[unlikely]] {
 				if (doBench()) {
 					doBench(false);
 					BVS.changeTitle(HDM.file.c_str());
+				#ifdef _WIN32
+					std::system("cls");
+				#else
+					std::system("clear");
+				#endif
 				} else {
 					doBench(true);
 					BVS.changeTitle(std::to_string(Guest.fetchCPF()));
-					std::cout
-						<< "\33[1;1H\33[2J"
-						<< "Cycle time:    .    ms\n"
-						<< "Time since last frame: ";
+					std::cout << "\33[1;1H\33[2J"
+						<< "Cycle time:    .    ms"
+						<< "\nTime since last frame: ";
 				}
 			}
 
-			if (kb.isPressed(KEY(PAGEDOWN))) {
+			if (kb.isPressed(KEY(PAGEDOWN))) [[unlikely]] {
 				BVS.changeFrameMultiplier(-1);
 			}
-			if (kb.isPressed(KEY(PAGEUP))) {
+			if (kb.isPressed(KEY(PAGEUP))) [[unlikely]] {
 				BVS.changeFrameMultiplier(+1);
 			}
 
-			if (doBench()) {
-				if (kb.isPressed(KEY(UP))) {
+			if (doBench()) [[likely]] {
+				if (kb.isPressed(KEY(UP))) [[unlikely]] {
 					BVS.changeTitle(std::to_string(Guest.changeCPF(+50'000)));
 				}
-				if (kb.isPressed(KEY(DOWN))) {
+				if (kb.isPressed(KEY(DOWN))) [[unlikely]] {
 					BVS.changeTitle(std::to_string(Guest.changeCPF(-50'000)));
 				}
 
@@ -113,17 +117,17 @@ bool VM_Host::runHost() {
 				if (Frame.getValidFrameCounter() & 0x1) {
 					const auto micros{ Frame.getElapsedMicrosSince() };
 					std::cout
-						<< "\33[2;25H"
-						<< Frame.getElapsedMillisLast()
 						<< "\33[1;12H"
 						<< std::setfill(' ') << std::setw(4) << micros / 1000
 						<< "\33[1C"
 						<< std::setfill('0') << std::setw(3) << micros % 1000
+						<< "\33[2;25H"
+						<< Frame.getElapsedMillisLast()
 						<< std::endl;
 				}
 			} else { Guest.processFrame(); }
 		} else {
-			if (kb.isPressed(KEY(ESCAPE))) {
+			if (kb.isPressed(KEY(ESCAPE))) [[unlikely]] {
 				return EXIT_SUCCESS;
 			}
 		}
@@ -139,8 +143,7 @@ void VM_Host::prepareGuest(VM_Guest& Guest, FrameLimiter& Frame) {
 	bic::kb.updateCopy();
 	bic::mb.updateCopy();
 
-	if (GameFileChecker::hasCore()) {
-		Guest.initGameCore(HDM, BVS, BAS);
+	if (Guest.initGameCore(HDM, BVS, BAS)) {
 		Frame.setLimiter(Guest.fetchFramerate());
 		BVS.changeTitle(HDM.file.c_str());
 	} else {
