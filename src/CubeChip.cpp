c6fa84b0069c9c0dd5ded19f3c89fbd7f1a0bd6f
Στέφανος "Coornio/8924th" Βλαστός <8924th@gmail.com>
2024-09-21 17:29:29 +0300

    Enforce C++20 in the cmake

--- src/CubeChip.cpp
+++ src/CubeChip.cpp
@@ -35,7 +35,7 @@ SDL_AppResult SDL_AppInit(void **Host, int argc, char* argv[]) {
 //             VS               OTHER
 #if !defined(NDEBUG) || defined(DEBUG)
 	{
-		printf("SDL3 test dated: 8/9/2024 (dmy)\n");
+		printf("SDL3 test dated: 15/9/2024 (dmy)\n");
 		const auto compiled{ SDL_VERSION };  /* hardcoded number from SDL headers */
 		const auto linked{ SDL_GetVersion() };  /* reported by linked SDL library */
 
